90be8e85f0fce5a19b1026b1a335215b6ad52b23
zarvish <codzviston@gmail.com>
2022-05-01 06:02:47 +0000

    Initial Commit

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic=[
+    {
+        name: "Ammi-Kmaal Khan",
+        artist:"Kmaal Khan",
+        img: "Ammi",
+        src: "music-1"
+    },
+    {
+        name: "Awaaz-Kmaal Khan",
+        artist:"Kmaal Khan",
+        img: "Awaaz",
+        src: "music-2"
+    },
+    {
+        name: "Duniya-Gurnam Bhullar",
+        artist:"Gurnam Bhullar",
+        img: "Duniya",
+        src: "music-3"
+    },
+    {
+        name: "Fakeera-Kmaal Khan",
+        artist:"Kmaal Khan",
+        img: "Fakira",
+        src: "music-4"
+    },
+    {
+        name: "Jaan Deyange-Kmaal Khan",
+        artist:"Ammy Virk",
+        img: "Jaan_deyange",
+        src: "music-5"
+    },
+    {
+        name: "Pariya-Gurnam Bhullar",
+        artist:"Gurnam Bhullar",
+        img: "Pariyan",
+        src: "music-6"
+    }
+]    
